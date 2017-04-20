b3620031fe9b40ab5f4833ccd65b260d1c9cffc1
Nigel <nigelwarmstrong@hotmail.com>
2017-04-20 15:49:03 -0400

    Include demo factory

---
+++ client/app/demo/demo-factory.js
@@ -0,0 +1,33 @@
+﻿(function (angular) {
+    var roiDemo = angular.module('roiDemo'); // one argument == look up module
+    // attach controller to a module moduleOb.controller('name', impl)
+    roiDemo.factory('roiGeo', ['$q', roiGeo]);
+
+    function roiGeo($q) {
+
+        return {
+            getCurrentPosition: getCurrentPosition
+        };
+        
+        function getCurrentPosition() {
+
+            
+
+                
+            var promise = $q(function (resolve, reject) {
+
+                navigator.geolocation.getCurrentPosition(function (pos) {
+                    resolve(pos);
+                }, function (err) {
+                    reject(err);
+                });
+
+
+
+            });
+
+            return promise;
+        }
+    }
+
+})(angular); // IIFE
