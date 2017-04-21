64652439daf4c00203d5e7de2bb779b82f485528
Nigel <nigelwarmstrong@hotmail.com>
2017-04-21 13:55:37 -0400

    Some jQuery and an Angular directive

---
+++ client/app/people/hello-directive.js
@@ -0,0 +1,21 @@
+﻿(function (angular) {
+    var roiPeople = angular.module('roiPeople'); 
+    
+    roiPeople.directive('roiHello', function () {
+        // DDO (Directive Definition Object)
+        return {
+            templateUrl: 'app/people/hello-directive-template.html',
+            //template: '<h1>Hello from AngularJS</h1>',
+            restrict: 'E',
+            replace: true, // probably don't use this...
+            controller: function ($scope, $interval) {
+                //$scope.format = 'hh:mm';
+                $interval(function () { $scope.time = new Date(); }, 1000);
+            },
+            scope: {
+                format: '@'
+            }
+        }
+    });
+
+})(angular); // IIFE
